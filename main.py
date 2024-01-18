4cfbe2f6464911023cd99ac71787df5951a44eba
elxp101t3r <eliotx10101@outlook.com>
2024-01-18 15:51:25 +0200

    Start the UI

--- main.py
+++ main.py
@@ -10,7 +10,19 @@ window.config(padx=20, pady=20)
 canvas = Canvas(height=200, width=200)
 logo = PhotoImage(file='logo.png')
 canvas.create_image(100, 100,image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+link_label = Label(text='Link', bootstyle='inverse-default')
+link_label.grid(column=0, row=1)
+link_text = Entry(bootstyle='success', width=35)
+link_text.grid(column=1, row=1, columnspan=2, padx=10)
+
+email_label = Label(text='Username or Email', bootstyle='inverse-success')
+email_label.grid(column=0, row=2)
+email_text = Entry(bootstyle='warning', width=35)
+email_text.grid(column=1, row=2, columnspan=2, pady=10)
+
+
 
 
 
