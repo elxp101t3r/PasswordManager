90c3c13e34e92887443041e255c79bc8a8515723
elxp101t3r <eliotx10101@outlook.com>
2024-01-18 15:26:25 +0200

    Init

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from ttkbootstrap import *
+from ttkbootstrap.constants import *
+from PIL import Image, ImageTk
+
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo = PhotoImage(file='logo.png')
+canvas.create_image(100, 100,image=logo)
+canvas.pack()
+
+
+
+window.mainloop()
